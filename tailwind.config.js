ad154bb3ed342427b3601a49c04acc85210a6fea
Christian Mendoza <christianmendozater@gmail.com>
2024-04-27 01:47:34 -0400

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    './src/**/*.{vue,js,ts,js,tsx}'
+  ],
   theme: {
     extend: {},
   },
