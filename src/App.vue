b0c23ccfb5811db268134a0e3917dfe9f4ca383e
Christian Mendoza <christianmendozater@gmail.com>
2024-05-01 23:09:45 -0400

    form created

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import Header from './components/Header.vue';
+import Formulario from './components/Formulario.vue';
+
 </script>
 
 <template>
   <!-- esto centtra el header -->
   <div class="container mx-auto mt-20">
-    <Header></Header>
+    <Header/>
+    <div class="mt-12 md:flex">
+      <formulario/>
+    </div>
   </div>
 </template>
