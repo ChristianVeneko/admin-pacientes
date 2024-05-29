00fe67f56e5d05c6516941fce4ea061c2c85141a
Christian Mendoza <christianmendozater@gmail.com>
2024-05-28 23:00:53 -0400

    Datos del paciente reactive para agruparlo mejor y el submit del formulario con una funcion definida pero no lista de validar

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,7 +1,17 @@
 <script setup>
-import { ref } from 'vue';
-const nombre = ref('Max')
+import { reactive } from 'vue';
+const paciente = reactive({
+    nombre: '',
+    propietario: '',
+    email: '',
+    alta: '',
+    sintomas: '',
+})
 
+const validar = () => {
+    
+    console.log(paciente)
+}
 
 </script>
 
@@ -12,17 +22,22 @@ const nombre = ref('Max')
             Añade Pacientes y
             <span class="text-indigo-600 font-bold">Administralos</span>
         </p>
-        <!-- shadow md agrega sobras, y rounded agrega border radius, px es padding en el eje x y py padding en ej ele y,  mb es margin button -->
-        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
-            {{ nombre }}
+        <!-- shadow md a    grega sobras, y rounded agrega border radius, px es padding en el eje x y py padding en ej ele y,  mb es margin button -->
+
+        <!--subimt.prevent es igual que  e.preventDefault() en la funcion validar-->
+        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+                @submit.prevent="validar"
+        >
             <div class="mb-5 ">
                 <label for="mascota" class="block text-gray-700 uppercase font-bold">
                     Nombre Mascota
                 </label>
                 <!--inline event @input le coloca el valor colocado nombre y ya que nombre esta puesto arriba pues se actualiza en tiempo real-->
+
+                <!--Una manera mucho mas sencilla es con v-model-->
                 <input name="text" id="mascota" placeholder="Nombre de la masctoa"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" :value="nombre"
-                    @input="(e) => nombre = e.target.value" />
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+                    v-model="paciente.nombre" />
 
             </div>
 
@@ -31,7 +46,8 @@ const nombre = ref('Max')
                     Nombre Propietario
                 </label>
                 <input name="text" id="propietario" placeholder="Nombre del propietario"
-                    class="border-2 w-full p-2 mt=2 placeholder-gray-400 rounded-md">
+                    class="border-2 w-full p-2 mt=2 placeholder-gray-400 rounded-md"
+                    v-model="paciente.propietario">
             </div>
 
             <div class="mb-5 ">
@@ -39,14 +55,16 @@ const nombre = ref('Max')
                     Email
                 </label>
                 <input id="email" type="email" placeholder="Email del propietario"
-                    class="border-2 w-full p-2 mt=2 placeholder-gray-400 rounded-md">
+                    class="border-2 w-full p-2 mt=2 placeholder-gray-400 rounded-md"
+                    v-model="paciente.email">
             </div>
 
             <div class="mb-5 ">
                 <label for="alta" class="block text-gray-700 uppercase font-bold">
                     Alta
                 </label>
-                <input id="alta" type="date" class="border-2 w-full p-2 mt=2 placeholder-gray-400 rounded-md">
+                <input id="alta" type="date" class="border-2 w-full p-2 mt=2 placeholder-gray-400 rounded-md"
+                v-model="paciente.alta">
             </div>
 
             <div class="mb-5 ">
@@ -54,7 +72,8 @@ const nombre = ref('Max')
                     Sintomas
                 </label>
                 <textarea id="sintomas" placeholder="Describe los sintomas del paciente"
-                    class="border-2 w-full p-2 mt=2 placeholder-gray-400 rounded-md h-40" />
+                    class="border-2 w-full p-2 mt=2 placeholder-gray-400 rounded-md h-40"
+                    v-model="paciente.sintomas" />
             </div>
             <input type="submit"
                 class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
