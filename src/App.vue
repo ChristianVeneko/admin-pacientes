8c542d0e145a62eb7438bf46443e93158b083ba4
Christian Mendoza <christianmendozater@gmail.com>
2024-07-16 00:29:49 -0400

    componente de pacientes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 import { reactive } from 'vue';
 import Header from './components/Header.vue';
 import Formulario from './components/Formulario.vue';
-
+import Paciente from './components/Paciente.vue';
 const pacientes = ref([]);
 
 
@@ -50,7 +50,11 @@ const guardarPaciente = () => {
       <div class="md:w-1/2 md:h-screen overflow-y-scroll">
         <h3 class="font-black text-3xl text-center ">Administra tus Pacientes</h3>
         <div v-if="pacientes.length > 0">
-
+          <p class="text-lg mt-5 text-center mb-10">
+            Informacion de
+            <span class="text-indigo-600 font-bold">Pacientes</span>
+        </p>
+          <Paciente v-for="paciente in pacientes" :paciente="paciente"/>
         </div>
         <p v-else class="text-2xl text-center mt-20">No hay pacientes</p>
       </div>
